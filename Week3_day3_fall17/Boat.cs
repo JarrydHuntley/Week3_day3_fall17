626b13f961800c3bfa173b6abbfd7fbf4830f22d
Jarryd Huntley <Jarryd Huntley>
2017-10-11 14:25:54 -0400

    Added in the Boat Class and constructor.

---
+++ Week3_day3_fall17/Boat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day3_fall17
+{
+    class Boat :Vehicle
+    {
+
+        private double waterDrag;
+
+        public Boat(int seats, int carryingCapacity, string color, double movementSpeed, double waterDrag)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+            this.waterDrag = waterDrag;
+        }
+    }
+}
